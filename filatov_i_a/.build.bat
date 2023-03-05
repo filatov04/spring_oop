b5d4e5b50114f6cfa187232b73674875ab77d952
filatov04 <filka04012004@yandex.ru>
2023-03-05 14:56:33 +0300

    new tree

---
+++ filatov_i_a/.build.bat
@@ -0,0 +1,2 @@
+cmake -S . -B build -DCMAKE_TOOLCHAIN_FILE=C:/src/vcpkg/scripts/buildsystems/vcpkg.cmake
+cmake --build build --config Release
---
+++ filatov_i_a/prj.test/RationalTest.cpp
@@ -0,0 +1,16 @@
+﻿#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest/doctest.h>
+#include "../prj.lab/Rational/include/Rational/Rational.h"
+#include <iostream>
+
+TEST_CASE("[rational] - Rational ctor") {
+	CHECK((Rational() == Rational(0, 1)));
+	CHECK((Rational(3) == Rational(3, 1)));
+	CHECK((Rational(-3) == Rational(-3, 1)));
+	CHECK((Rational(10, 6) == Rational(5, 3)));
+	CHECK((Rational(-10, 6) == Rational(-5, 3)));
+	CHECK((Rational(10, -6) == Rational(-5, 3)));
+	CHECK((Rational(-10, -6) == Rational(5, 3))); 
+
+	//CHECK_THROWS(Rational(1, 0));
+}
